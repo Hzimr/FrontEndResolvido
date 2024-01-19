2e532511ef25fc4cfcaf0ab9bc4df9551c11810a
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-19 19:32:27 -0300

    feat: created header (lembrar de colocar o localsStorage no browser)

--- src/components/primary-input.tsx
+++ src/components/primary-input.tsx
@@ -1,14 +1,45 @@
 'use client'
 
 import styled from "styled-components"
+import { SearchIcon } from "./search-icon"
+import { InputHTMLAttributes } from "react"
 
-const PrimaryInput = styled.input`
-  position: absolute;
-  width: 22rem;
-  height: 2.5rem;
-  left: 871px;
-  top: 19px;
-
-  background-color: #F3F5F5;
+export const PrimaryInput = styled.input`
+  width: 352px;
   border-radius: 8px;
+  padding: 10px 16px;
+  border: none;
+
+  background-color: var(--bg-secondary);
+
+  font-family: inherit;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: var(--text-dark);
 `
+
+const InputContainer = styled.div`
+  position: relative;
+  width: 352px;
+
+  svg {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+
+}
+
+export function PrimaryInputWithSearchIcon(props: InputProps) {
+  return (
+    <InputContainer>
+      <PrimaryInput {...props}/>
+      <SearchIcon />
+    </InputContainer>
+  )
+}
