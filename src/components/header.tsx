e91731cefd183aceab1cb08883e47297dcaa3b5e
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-19 16:14:09 -0300

    chore: changed projet npm to Pnpm

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { styled } from "styled-components"
+
+import { Saira_Stencil_One } from 'next/font/google'
+
+const sairaStencil = Saira_Stencil_One({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+interface HeaderProps {
+
+}
+
+const TagHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.25rem 10rem;
+`
+
+const Logo = styled.a`
+  color: var(--logo-color);
+  font-weight: 400;
+  font-size: 2.5rem;
+  line-height: 150%;
+`
+
+export function Header(props: HeaderProps){
+  return(
+    <TagHeader>
+      <Logo className={sairaStencil.className}>Capputeeno</Logo>
+      <div></div>
+    </TagHeader>
+  )
+}
---
+++ src/components/primary-input.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import styled from "styled-components"
+
+const PrimaryInput = styled.input`
+  position: absolute;
+  width: 22rem;
+  height: 2.5rem;
+  left: 871px;
+  top: 19px;
+
+  background-color: #F3F5F5;
+  border-radius: 8px;
+`
