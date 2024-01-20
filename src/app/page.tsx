f9e7264a9e6557a403284de58b4e4cae3bee5ec3
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-20 00:31:39 -0300

    feat: initial products fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from 'next/image'
+'use client'
+import { ProductsList } from '@/components/products-list'
 import styles from './page.module.css'
+import { FilterBar } from '@/components/filter-bar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
+  const client = new QueryClient();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>Olá Novo Projeto</h1>
-      </div>
-    </main>
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar/>
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
   )
 }
